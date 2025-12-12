da8e9ba9c44d8d0b4dbebcb92dbf49f4b9f563c8
merwane782 <merw.mallem@gmail.com>
2025-12-12 17:28:57 +0100

    Add cargo commands for formatting and running
    Added commands for formatting, linting, and running the application with various arguments.

--- rust_00/src/main.rs
+++ rust_00/src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
-}
+cargo fmt
+cargo clippy -- -D warnings
+cargo run
+cargo run -- merwane
+cargo run -- --upper merwane
+cargo run -- --repeat 3 merwane
